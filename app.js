c57566f5fcf3d869fbbddde6069c2abf346bf9a7
Hursh <hsa470@gmail.com>
2011-07-12 20:38:39 -0400

    added google oauth

--- app.js
+++ app.js
@@ -3,25 +3,33 @@
  */
 
 var express = require('express'),
-	sys 	= require('sys');
+	sys 	= require('sys'),
+	oauth	= require('./node-oauth/lib/oauth').OAuth;
 
 // Redis database configuration
 if (process.env.REDISTOGO_URL) {  //for heroku redisToGo
 	var rtg   = require("url").parse(process.env.REDISTOGO_URL);
-	var redis = require("redis").createClient(rtg.port, rtg.hostname);
+	var redis = require("redis");
+	var client = redis.createClient(rtg.port, rtg.hostname);
 	
-	console.log(rtg.auth.split(":")[1]);
+	//console.log(rtg.auth.split(":")[1]);
 	redis.auth(rtg.auth.split(":")[1]);
 } else {
-	var redis = require("redis").createClient();
+	var redis = require("redis");
+	var client = redis.createClient();
 }
 
 var RedisStore = require('connect-redis')(express);
 
-redis.on("error", function(err){
+client.on("error", function(err){
 	console.log("Error " + err);
 });
+
 //Google config
+var googleoa = new oauth("https://www.google.com/accounts/OAuthGetRequestToken",
+             "https://www.google.com/accounts/OAuthAuthorizeToken", 
+             "optimize.heroku.com",  "rJjoyI8mcoZqxpTGRXkiRn4U", 
+             "1.0", null, "HMAC-SHA1");       
 
 //Facebook config
 
@@ -55,15 +63,35 @@ app.error(function(err, req, res, next){
 
 // Routes
 app.get('/', function(req, res){
-	if (req.session.visits) req.session.visits++;
-	else req.session.visits = 1;
-	
 	res.render('index', {
-		title: '' + req.session.visits
+		title: "Optimize"
 	});
 });
+
+app.get('/getGoogleRequestToken', function(req, res) {
+	getGoogleRequestToken(req, res);
+});
+
+
 
 var port = process.env.PORT || 3000;
 app.listen(port, function(){
   console.log("Listening on " + port);
 });
+
+
+var getGoogleRequestToken = function(req, res) {
+	googleoa.getOAuthRequestToken({"scope":"http://www.google.com/calendar/feeds"}, function(error, oauth_token, oauth_token_secret, oauth_callback_confirmed, results) {
+		if (error) {
+			res.send('error: ' + JSON.stringify(error));
+		} else {
+			res.redirect('https://www.google.com/accounts/OAuthAuthorizeToken?oauth_token=' + oauth_token);
+			
+			sys.puts('oauth_token: ' + oauth_token);
+			client.set(req.sessionID + ':google:requestToken', oauth_token, redis.print);
+			sys.puts('oauth_token_secret: ' + oauth_token_secret);
+			client.set(req.sessionID + ':google:requestTokenSecret', oauth_token_secret, redis.print);
+			sys.puts("Requesting access token...");
+		}
+	});
+};
