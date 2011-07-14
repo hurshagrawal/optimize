846cf7e3a5e73c6fcd61738fe07e4ac2e89c74fe
Hursh <hsa470@gmail.com>
2011-07-14 17:35:12 -0400

    google calendar query

--- app.js
+++ app.js
@@ -118,7 +118,6 @@ var getGoogleAccessToken = function(req, res) {
 //					console.log(oauth_access_token_secret);
 					client.set(req.sessionID+':google:accessToken', oauth_access_token, redis.print);
 					client.set(req.sessionID+':google:accessTokenSecret', oauth_access_token_secret, redis.print);
-					console.log("1");
 					getGoogleCalendarList(req, res);
 				}
 			});
@@ -130,9 +129,9 @@ var getGoogleCalendarList = function(req, res) {
 				req.sessionID + ':google:accessTokenSecret', 
 		function(err, replies) {
 			console.log("access token: "+replies[0]); //access token
-			console.log("access token secret"+replies[1]); //access token secret
+			console.log("access token secret: "+replies[1]); //access token secret
 			var requestUrl = "https://www.google.com/calendar/feeds/default/allcalendars/full?alt=jsonc";
-			googleoa.get(requestUrl, replies[0], replies[1], function(error, results) {
+			googleoa.get(requestUrl, replies[0], replies[1], function(error, null, results) {
 				if (error) {
 					sys.puts('error: ' + sys.inspect(error));
 				} else {
