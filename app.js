77c1ace3fbb465956541cf604e0fdd40edbf2ce5
Hursh <hsa470@gmail.com>
2011-07-26 17:45:05 -0400

    refactoring to use ajax

--- app.js
+++ app.js
@@ -31,6 +31,7 @@ var app = module.exports = express.createServer();
 app.configure(function(){
 	app.set('views', __dirname + '/views');
 	app.set('view engine', 'ejs');
+	app.set('view options', { layout: false });
 	app.use(express.bodyParser());
 	app.use(express.methodOverride());
 	app.use(express.cookieParser());
